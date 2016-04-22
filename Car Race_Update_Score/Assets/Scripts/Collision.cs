1ee6e52f1242639ea43ce515cf6c03d8fed1dda0
julienyaho <julienyaho92@hotmail.com>
2016-04-22 19:25:54 +0100

    update

---
+++ Car Race_Update_Score/Assets/Scripts/Collision.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sound : MonoBehaviour 
+{
+	/*void OnCollisionExit(Collision col)
+	{
+		Debug.Log (gameObject.name = " has collided with wall" = col.gameObject.name);
+	}*/
+	/*void OnCollisionStay(Collision col)
+	{
+		if (!audio.UnityEngine.Component && col.relativeVelocity.magnitude >= 2) {
+			audio.volume = col.relativeVelocity.magnitude / 5;
+			audio.play ();
+		}
+	}*/
+
+}
